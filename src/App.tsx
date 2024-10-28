9435b03ef9e3a1d0b9f0bcabcb1cedb24cf4b468
sneh333 <puran1430@gmail.com>
2024-10-28 13:26:54 +0530

    gh

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,22 @@ import './App.css';
 
 function App() {
   return (
-const array1 = ['a', 'b', 'c'];
-const array2 = ['d', 'e', 'f'];
-const array3 = array1.concat(array2);
-
-console.log(array3);
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React!!
+        </a>
+      </header>
+    </div>
   );
 }
 
