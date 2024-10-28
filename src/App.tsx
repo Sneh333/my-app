7713068323084487d32d10b16d073a055609f738
sneh333 <puran1430@gmail.com>
2024-10-28 13:30:50 +0530

    msg

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+console.log(array3);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!
+          Learn React!!!
         </a>
       </header>
     </div>
